385b974389038c4046e03caec72355a42dbf8905
Marco <5788988+marc0janssen@users.noreply.github.com>
2021-12-24 10:01:31 +0100

    release 1.0-0.12.1

--- build.sh
+++ build.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 
-# Name: restic-backup-helper
-# Coder: Marco Janssen (twitter @marc0janssen)
-# date: 2021-12-20 17:01:55
-# update: 2021-12-20 17:02:00
-
-docker image rm marc0janssen/restic-backup-helper:latest
-docker build -t marc0janssen/restic-backup-helper:latest -f ./Dockerfile .
+docker build --rm -t restic-backup .
